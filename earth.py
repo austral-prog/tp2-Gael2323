708fa425182c23818957397c44ef97f22babe465
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-08-21 21:16:01 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,5 +2,5 @@ def earth():
     #En este ejercicio se aplican los booleanos, que basicamente son los comparativos que te devuelven true or false. ademas de usar los f string que permiten añadir variables en un print
     x = "Bangladesh"
     y = "Barbados"
-    print(f'The result of {x} comes first in the dictionary than Y is {x < y}.')
-    print(f'The result of {y} comes first in the dictionary than X is {y < x}.')
+    print(f'The result of {x} comes first in the dictionary than Barbados is {x < y}.')
+    print(f'The result of {y} comes first in the dictionary than Banglades is {y < x}.')
