3ace98dfc8639d59f4aeba442ce8c2888a0e760b
Gael2323 <lgherrera@mail.austral.edu.ar>
2025-08-29 12:17:26 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,4 +1,20 @@
 def change():
     expense = 23.75
     money = 100
-    print(f'Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n\nVuelto\n\nPesos:\n{int(money) - int(expense) -1 }\nCentavos:\n{(int(money) - int(expense)) - 52 }'  )
+    vuelto = 0
+    centavos = 0
+    # Intento fallido print(f'Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n\nVuelto\n\nPesos:\n{int(money) - int(expense) -1 }\nCentavos:\n{(int(money) - int(expense)) - 52 }'  )
+    print(f"Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+
+    print("\nVuelto\n")
+
+    print("Pesos:")
+    vuelto = int(money - expense)
+    print(int(vuelto))
+
+    print("Centavos: ")
+    centavos = ((money - expense)-vuelto) * 100
+    print(int(centavos))
